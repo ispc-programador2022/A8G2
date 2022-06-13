3353fc97f823170fd600aba2fbc4af1909895711
maxluna12 <78857305+maxluna12@users.noreply.github.com>
2022-06-13 03:03:45 -0300

    push

---
+++ Suma.py
@@ -0,0 +1,3 @@
+def suma(a,b):
+    retur (a + b)
+    
---
+++ funcion_p1.py
@@ -0,0 +1,7 @@
+import suma
+import producto
+def p1(a,b,c):
+    sum = suma.suma(a,b)
+    multiplicacion = producto.producto(sum, c)
+    return (mutiplicacion)
+    
