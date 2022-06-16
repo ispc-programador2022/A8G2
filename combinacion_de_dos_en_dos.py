693d0a8a5fb02b8f8bda5e31f885465348a81549
maxluna12 <78857305+maxluna12@users.noreply.github.com>
2022-06-15 22:39:03 -0300

    Add files via upload

---
+++ combinacion_de_dos_en_dos.py
@@ -0,0 +1,10 @@
+ import funcion_genrnd
+
+ def sumgen():
+    lista = genrnd()
+    total = 0
+    for i in range(0,len(lista)-1):
+        for j in range(i + 1,len(lista)):
+            total += lista[i] + lista[j]
+    return total        
+          
