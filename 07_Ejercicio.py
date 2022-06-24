2ae6db370f27283de581ef96541a5114f089cc14
maxluna12 <78857305+maxluna12@users.noreply.github.com>
2022-06-24 00:01:50 -0300

    Create 07_Ejercicio.py

---
+++ 07_Ejercicio.py
@@ -0,0 +1,13 @@
+#Funcion Radicacion 
+""" calcula la raiz cuadrada de un numero"""
+"""En Python ya vienen incorporados funciones y operadores para sacar la raiz cuadrada"""
+
+import math
+print(math.sqrt(16))
+4.0
+
+#Otra funcion es POW()
+"""El primer parametro de la funcio pow() es la base y el seundo el exponente"""
+
+print(pow(9,0.5))
+3.0
